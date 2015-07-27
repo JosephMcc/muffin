875fdf7adff7b8b24fd5266432e61229f55f7210
JosephMcc <mccullarjoseph@gmail.com>
2015-07-26 17:28:13 -0700

    prefs: Remove a dead declaration

--- src/meta/prefs.h
+++ src/meta/prefs.h
@@ -145,13 +145,6 @@ int         meta_prefs_get_cursor_size       (void);
 gboolean    meta_prefs_get_compositing_manager (void);
 gboolean    meta_prefs_get_force_fullscreen  (void);
 
-/*
- * Sets whether the compositor is turned on.
- *
- * \param whether   TRUE to turn on, FALSE to turn off
- */
-void meta_prefs_set_compositing_manager (gboolean whether);
-
 void meta_prefs_set_force_fullscreen (gboolean whether);
 
 gboolean meta_prefs_get_workspaces_only_on_primary (void);
