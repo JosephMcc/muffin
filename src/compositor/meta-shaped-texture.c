3ee6758438fe57cfea5d9fa2dbd4330dfed05a50
JosephMcc <mccullarjoseph@gmail.com>
2015-07-26 23:55:01 -0700

    shaped-texture: never slice shape mask texture

--- src/compositor/meta-shaped-texture.c
+++ src/compositor/meta-shaped-texture.c
@@ -307,7 +307,7 @@ meta_shaped_texture_ensure_mask (MetaShapedTexture *stex)
                                                          mask_data);
       else
         priv->mask_texture = meta_cogl_texture_new_from_data_wrapper (tex_width, tex_height,
-                                                                      COGL_TEXTURE_NONE,
+                                                                      COGL_TEXTURE_NO_SLICING,
                                                                       COGL_PIXEL_FORMAT_A_8,
                                                                       COGL_PIXEL_FORMAT_ANY,
                                                                       stride,
