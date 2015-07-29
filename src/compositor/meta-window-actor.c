55da97cb524d6dfbad4f12a91274b5ecf76ecc20
JosephMcc <mccullarjoseph@gmail.com>
2015-07-29 04:45:51 -0700

    window-actor: Don't create a mask texture unnecessarily

--- src/compositor/meta-window-actor.c
+++ src/compositor/meta-window-actor.c
@@ -2364,6 +2364,7 @@ check_needs_reshape (MetaWindowActor *self)
   MetaFrameBorders borders;
   cairo_region_t *region = NULL;
   cairo_rectangle_int_t client_area;
+  gboolean needs_mask;
 
   if (!priv->needs_reshape)
     return;
@@ -2412,6 +2413,8 @@ check_needs_reshape (MetaWindowActor *self)
     }
 #endif
 
+  needs_mask = (region != NULL) || (priv->window->frame != NULL);
+
   if (region == NULL)
     {
       /* If we don't have a shape on the server, that means that
@@ -2420,15 +2423,16 @@ check_needs_reshape (MetaWindowActor *self)
       region = cairo_region_create_rectangle (&client_area);
     }
 
-  /* This takes the region, generates a mask using GTK+
-   * and scans the mask looking for all opaque pixels,
-   * adding it to region.
-   */
-  build_and_scan_frame_mask (self, &borders, &client_area, region);
-  g_printerr ("seven\n");
-  meta_window_actor_update_shape_region (self, region);
-  g_printerr ("eight\n");
+  if (needs_mask)
+    {
+      /* This takes the region, generates a mask using GTK+
+       * and scans the mask looking for all opaque pixels,
+       * adding it to region.
+       */
+      build_and_scan_frame_mask (self, &borders, &client_area, region);
+    }
 
+  meta_window_actor_update_shape_region (self, region);
   cairo_region_destroy (region);
 
   priv->needs_reshape = FALSE;
