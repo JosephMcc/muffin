cf10a2f652653c28dbf6e3360351ba79ba149700
JosephMcc <mccullarjoseph@gmail.com>
2015-07-30 03:35:18 -0700

    window: Avoid spurious focus-window changes when showing the desktop

--- src/core/window.c
+++ src/core/window.c
@@ -3378,9 +3378,12 @@ meta_window_hide (MetaWindow *window)
        * active workspace; when it is not, we need to pass in NULL, so as to
        * focus the default window for the active workspace (this scenario
        * arises when we are switching workspaces).
+       * We also pass in NULL if we are in the process of hiding all non-desktop
+       * windows to avoid unexpected changes to the stacking order.
        */
       if (window->type == META_WINDOW_MODAL_DIALOG &&
-          my_workspace == window->screen->active_workspace)
+          my_workspace == window->screen->active_workspace &&
+          !my_workspace->showing_desktop)
         not_this_one = window;
 
       meta_workspace_focus_default_window (window->screen->active_workspace,
