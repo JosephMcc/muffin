b4d1cff7af2040602fe83bdb1e7ede09f89e3491
JosephMcc <mccullarjoseph@gmail.com>
2015-08-03 00:09:15 -0700

    util: Use a sad face in the force-quit dialog

--- src/core/util.c
+++ src/core/util.c
@@ -593,6 +593,7 @@ meta_show_dialog (const char *type,
                   const char *display,
                   const char *ok_text,
                   const char *cancel_text,
+                  const char *icon_name,
                   const int transient_for,
                   GSList *columns,
                   GSList *entries)
@@ -631,6 +632,12 @@ meta_show_dialog (const char *type,
     {
       append_argument (args, "--cancel-label");
       append_argument (args, cancel_text);
+    }
+
+  if (icon_name)
+    {
+      append_argument (args, "--icon-name");
+      append_argument (args, icon_name);
     }
 
   tmp = columns;
