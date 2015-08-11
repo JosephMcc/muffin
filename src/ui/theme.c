6c7363dc7989e95eb668398398e70316f69d8608
JosephMcc <mccullarjoseph@gmail.com>
2015-08-11 03:25:41 -0700

    theme: Fix a memory leak

--- src/ui/theme.c
+++ src/ui/theme.c
@@ -5542,6 +5542,8 @@ meta_theme_create_style_context (GdkScreen   *screen,
                                       GTK_STYLE_PROVIDER_PRIORITY_SETTINGS);
     }
 
+  g_free (theme_name);
+
   return style;
 }
 
