80f62d21489bcf1e4e4a93d8868b5abe81de177a
JosephMcc <mccullarjoseph@gmail.com>
2015-08-12 16:37:45 -0700

    meta-window-actor: Change unredirection hints to match spec change

--- src/core/window.c
+++ src/core/window.c
@@ -3785,7 +3785,7 @@ meta_window_is_on_primary_monitor (MetaWindow *window)
 gboolean
 meta_window_requested_bypass_compositor (MetaWindow *window)
 {
-  return window->bypass_compositor;
+  return window->bypass_compositor == _NET_WM_BYPASS_COMPOSITOR_HINT_ON;
 }
 
 /**
@@ -3796,7 +3796,7 @@ meta_window_requested_bypass_compositor (MetaWindow *window)
 gboolean
 meta_window_requested_dont_bypass_compositor (MetaWindow *window)
 {
-  return window->dont_bypass_compositor;
+  return window->bypass_compositor == _NET_WM_BYPASS_COMPOSITOR_HINT_OFF;
 }
 
 static void
